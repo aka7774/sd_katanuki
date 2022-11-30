81a55e007ac0f27683913f6cd8e838cf37c658f1
aka7774 <aka7774@users.noreply.github.com>
2022-11-30 21:07:43 +0900

    Add Direct Download isnetis.ckpt

--- install.py
+++ install.py
@@ -1,15 +1,40 @@
 import os
 import pathlib
+import urllib.request
 import git
 
 from launch import git_clone
+from modules.devices import get_optimal_device
 
 p = pathlib.Path(__file__).parts[-3:-1]
 
-name = 'anime-seg'
-dir = f"{p[0]}/{p[1]}/{name}"
-if os.path.exists(dir):
-    git.cmd.Git(dir).pull()
-else:
+# 1. git clone
+dst = os.path.join(p[0], p[1], 'anime-seg', 'isnetis.ckpt')
+if not os.path.exists(dst):
+    name = 'anime-seg'
+    dir = f"{p[0]}/{p[1]}/{name}"
     url = 'https://huggingface.co/skytnt/anime-seg'
     git_clone(url, dir, name)
+
+# 2. check ckpt
+try:
+    device = get_optimal_device()
+    class Opt(object):
+        def __init__(self):
+            self.net = 'isnet_is'
+            self.ckpt = f"{p[0]}/{p[1]}/anime-seg/isnetis.ckpt"
+            self.device = str(device)
+    opt = Opt()
+
+    import importlib
+    train = importlib.import_module(f"{p[0]}.{p[1]}.anime-segmentation.train")
+    AnimeSegmentation = getattr(train, 'AnimeSegmentation')
+    model = AnimeSegmentation.try_load(opt.net, opt.ckpt, opt.device)
+except:
+# 3. direct download
+    url = 'https://huggingface.co/skytnt/anime-seg/resolve/main/isnetis.ckpt'
+    dst_dir = f"{p[0]}/{p[1]}/anime-seg"
+    dst = dst_dir + "/isnetis.ckpt"
+    pathlib.Path(dst_dir).mkdir(parents=True,exist_ok=True)
+    urllib.request.urlretrieve(url, dst)
+    print(f"Downloaded {url}")

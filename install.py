be8dc998d5ba405c1bd1ba985ab4aa6d710a6853
aka7774 <aka7774@users.noreply.github.com>
2022-12-17 18:44:53 +0900

    Add checked flag

--- install.py
+++ install.py
@@ -17,24 +17,27 @@ if not os.path.exists(dst):
     git_clone(url, dir, name)
 
 # 2. check ckpt
-try:
-    device = get_optimal_device()
-    class Opt(object):
-        def __init__(self):
-            self.net = 'isnet_is'
-            self.ckpt = f"{p[0]}/{p[1]}/anime-seg/isnetis.ckpt"
-            self.device = str(device)
-    opt = Opt()
+checked_path = os.path.join(p[0], p[1], 'isnetis.ckpt.checked')
+if not os.path.exists(checked_path):
+    try:
+        device = get_optimal_device()
+        class Opt(object):
+            def __init__(self):
+                self.net = 'isnet_is'
+                self.ckpt = f"{p[0]}/{p[1]}/anime-seg/isnetis.ckpt"
+                self.device = str(device)
+        opt = Opt()
 
-    import importlib
-    train = importlib.import_module(f"{p[0]}.{p[1]}.anime-segmentation.train")
-    AnimeSegmentation = getattr(train, 'AnimeSegmentation')
-    model = AnimeSegmentation.try_load(opt.net, opt.ckpt, opt.device)
-except:
+        import importlib
+        train = importlib.import_module(f"{p[0]}.{p[1]}.anime-segmentation.train")
+        AnimeSegmentation = getattr(train, 'AnimeSegmentation')
+        model = AnimeSegmentation.try_load(opt.net, opt.ckpt, opt.device)
+        pathlib.Path(checked_path).write_text('1')
+    except:
 # 3. direct download
-    url = 'https://huggingface.co/skytnt/anime-seg/resolve/main/isnetis.ckpt'
-    dst_dir = f"{p[0]}/{p[1]}/anime-seg"
-    dst = dst_dir + "/isnetis.ckpt"
-    pathlib.Path(dst_dir).mkdir(parents=True,exist_ok=True)
-    urllib.request.urlretrieve(url, dst)
-    print(f"Downloaded {url}")
+        url = 'https://huggingface.co/skytnt/anime-seg/resolve/main/isnetis.ckpt'
+        dst_dir = os.path.join(p[0], p[1], 'anime-seg')
+        dst = dst_dir + "/isnetis.ckpt"
+        pathlib.Path(dst_dir).mkdir(parents=True,exist_ok=True)
+        urllib.request.urlretrieve(url, dst)
+        print(f"Downloaded {url}")
